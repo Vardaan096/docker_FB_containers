03689b9e4102840a8bacf89ff89c22b72d46f5fd
vardaansharma <vardaansharma096@gmail.com>
2023-09-01 18:24:13 +0530

    commiting the project directory

---
+++ project/docker_shell_script.sh
@@ -0,0 +1,5 @@
+#! /bin/bash
+
+cd /home/antino/Desktop/project
+#The --build flag tells Docker Compose to rebuild the images used by your services, even if they already exist.
+sudo docker compose up -d --build
