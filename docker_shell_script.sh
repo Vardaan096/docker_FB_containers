8265b3e2266379feaf5b151aa15be4ac6d5af5a8
vardaansharma <vardaansharma096@gmail.com>
2023-09-08 19:14:40 +0530

    change in shell script

--- docker_shell_script.sh
+++ docker_shell_script.sh
@@ -2,4 +2,4 @@
 
 cd /home/antino/Desktop/project
 #The --build flag tells Docker Compose to rebuild the images used by your services, even if they already exist.
-sudo docker compose up -d --build
+sudo docker-compose up -d --build
